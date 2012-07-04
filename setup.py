ccc87770d5e88371b4baf3d17d14ce196c9ae093
Andy Dirnberger <dirn@dirnonline.com>
2012-07-03 20:47:57 -0400

    Begin work on version 0.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.argv[-1] == 'publish':
 
 settings.update(
     name='switches',
-    version='0.1.0',
+    version='0.2.0',
     description='Friendly Command Line Scripts',
     long_description=open('README.rst').read(),
     author='Andy Dirnberger',
