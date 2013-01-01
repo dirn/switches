a6329653df07d65ff04285bc03c73b605c98e1fc
Andy Dirnberger <dirn@dirnonline.com>
2013-01-01 14:38:00 -0500

    bump year

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'switches'
-copyright = u'2012, Andy Dirnberger'
+copyright = u'2013, Andy Dirnberger'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
